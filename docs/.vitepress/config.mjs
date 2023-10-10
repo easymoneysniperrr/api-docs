72c14211a115460b85cb8b52d260b864875cddd1
ouyangchengjun <ouyangchengjun@talkweb.com.cn>
2023-10-10 10:30:34 +0800

    添加logo

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,17 +3,18 @@
  * @Author: 欧阳承珺
  * @LastEditors: 欧阳承珺
  * @Date: 2023-10-09 18:03:04
- * @LastEditTime: 2023-10-10 09:36:33
+ * @LastEditTime: 2023-10-10 10:22:32
  */
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/api-docs/',
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  title: "在鸿云文档",
+  description: "在鸿云-api文档",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
